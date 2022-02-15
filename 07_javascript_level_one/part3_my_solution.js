0c6527948574d6702ffd9f3cb27b8d1387ac51b9
cristian restrepo <crestrepoq@unal.edu.co>
2022-02-14 22:48:16 -0500

    feature: starting Javascript module

---
+++ 07_javascript_level_one/part3_my_solution.js
@@ -0,0 +1,4 @@
+var pounds = prompt("Input number of pounds you want to know in kilograms");
+var kg = pounds*0.454;
+alert(pounds + " pounds are: " + kg + " kg");
+console.log("Conversion completed");
---
+++ 07_javascript_level_one/part_4_operators.js
@@ -0,0 +1,75 @@
+// Boleans
+console.log('true');
+console.log(true);
+console.log('false');
+console.log(false);
+
+console.log('3 > 2');
+console.log(3 > 2);
+
+console.log('2 > 3');
+console.log(2 > 3);
+
+console.log('1 > 2');
+console.log(1 > 2);
+
+console.log('2 == 2');
+console.log(2 == 2);
+
+console.log("'username' > 'username'");
+console.log("username" > "username");
+
+console.log("'2' == 2");
+console.log("2" == 2);
+
+// For be secure that is really different
+console.log("'2' === 2");
+console.log("2" === 2);
+
+console.log("2 ==! 3");
+console.log(2 ==! 3);
+
+console.log("5 ==! '5'");
+console.log(5 ==! '5');
+
+console.log("true == 1");
+console.log(true == 1);
+
+console.log("true === 1");
+console.log(true === 1);
+
+console.log("false == 0");
+console.log(false == 0);
+
+console.log("false === 0");
+console.log(false === 0);
+
+console.log("null == undefined");
+console.log(null == undefined);
+
+console.log("null === undefined");
+console.log(null === undefined);
+
+console.log("NaN == NaN");
+console.log(NaN == NaN);
+
+console.log("1 === 1 && 2 === 2");
+console.log(1 === 1 && 2 === 2);
+
+console.log("1 === 1 && 2 === 2 && 1 === 2");
+console.log(1 === 1 && 2 === 2 && 1 === 2);
+
+console.log("1 === 2 || 1 === 1");
+console.log(1 === 2 || 1 === 1);
+
+console.log("(1===1)");
+console.log((1===1));
+
+console.log("!(1===1)");
+console.log(!(1===1));
+
+console.log("!!(1===1)");
+console.log(!!(1===1));
+
+console.log("!!!!!(1===1)");
+console.log(!!!!!(1===1));
